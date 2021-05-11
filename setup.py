b7486ea12e844a8c13a513f7999953db8af1470b
Dafydd Jones <dafydd@techneg.it>
2021-05-11 15:58:30 +0100

    Mirror: 0.7.0

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ from setuptools import setup
 setup(
     name='pre_commit_dummy_package',
     version='0.0.0',
-    install_requires=['restructuredtext-lint==0.6.0'],
+    install_requires=['restructuredtext-lint==0.7.0'],
 )
